e1a8ed89285fd826559817ce9a557849a42a02fb
Altobert <55031078+Altobert@users.noreply.github.com>
2021-04-14 18:04:15 -0400

    Se agrega imagen sii

---
+++ pages/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="login-page">
+        <section class="section">
+            <div class="container">
+                <div>
+                    <a href="https://www.sii.cl"
+                    ><img src="@/assets/img/logo-white.svg" alt="SII"/></a>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
